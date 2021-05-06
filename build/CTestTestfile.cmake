7c1d9294901a682d7c696681186cc797d4bd851a
Eduardo Zhizhpon <eddzhizhpon@gmail.com>
2021-05-06 11:53:55 -0500

    bugs fixes

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Documentos/MEGAsync/Universidad/8 Ciclo/Vision por Computador/Trabajos/Practica1.2-3
-# Build directory: E:/Documentos/MEGAsync/Universidad/8 Ciclo/Vision por Computador/Trabajos/Practica1.2-3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
